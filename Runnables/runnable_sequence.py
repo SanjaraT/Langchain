f9ad5f1f47c288ff8ce17c462005428229c28ed2
sanjaraT <sanjaratunola@gmail.com>
2026-06-03 19:48:58 +0600

    Runnable Sequence

---
+++ Runnables/runnable_sequence.py
@@ -0,0 +1,29 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOllama(model="phi3")
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(
+    prompt1,
+    model,
+    parser,
+    prompt2,
+    model,
+    parser
+)
+
+print(chain.invoke({'topic': 'My computer science degree certificate'}))
