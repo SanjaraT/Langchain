d88e78886890c60213739618026ae581e89c9a33
sanjaraT <sanjaratunola@gmail.com>
2026-06-03 21:33:34 +0600

    Runnable parallel

---
+++ Runnables/runnable_parallel.py
@@ -0,0 +1,39 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel
+
+prompt1 = PromptTemplate(
+    template='Generate a tweet about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a Linkedin post about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOllama(model="phi3")
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'tweet': RunnableSequence(
+        prompt1,
+        model,
+        parser
+    ),
+    'linkedin': RunnableSequence(
+        prompt2,
+        model,
+        parser
+    )
+})
+
+result = parallel_chain.invoke({
+    'topic': 'AI'
+})
+
+print(result['tweet'])
+print()
+print(result['linkedin'])
